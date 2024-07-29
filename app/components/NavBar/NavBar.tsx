f95e585f364d037eed44c447c6429a0b9ae85de0
shelton <damwcs@gmail.com>
2024-07-29 22:04:34 +1000

    feat: optimize navbar layout

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -36,10 +36,10 @@ const NavBar = () => {
     <nav
       className={`shadow-2xl text-white pl-5 pr-5 sm:pr-10 py-8 flex justify-between fixed top-0 w-full transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'} bg-bg z-50`}
     >
-      <Link href={`#Hero`} className="transition md:translate-x-5">
+      <Link href={`#Hero`} className="transition sm:translate-x-5">
         <Image alt="loge" src="/logo.png" width={45} height={45} />
       </Link>
-      <div className="flex gap-x-5 justify-between font-mono text-sm items-center">
+      <div className="flex sm:gap-x-5 gap-x-3 justify-between font-mono text-sm items-center">
         {taps.map((tap, index) => {
           return (
             <Link
